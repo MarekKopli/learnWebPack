d87a13872804de1887da82dc7f4954756c99fec9
Marek Kopli <marek.kopli@tptlive.ee>
2022-10-05 12:17:03 +0300

    tan

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
-console.log('hello world!');
+import './style.scss';
 
-import hello from './hello.js';
-hello.hello();
+import { createApp } from 'vue';
+
+import * as VueRouter from 'vue-router';
+
+import App from './App.vue';
+import ToDoList from './pages/ToDoList.vue';
+import Modals from './pages/Modals.vue';
+import Clicker from './pages/Clicker.vue';
+import Chuck from './pages/Chuck.vue';
+import RickAndMorty from './pages/RickAndMorty.vue'
+
+const routes = [
+    { path: '/', component: RickAndMorty, name: 'Rick and Morty' }, 
+    { path: '/chuck', component: Chuck, name: 'Chuck' },  
+    { path: '/clicker', component: Clicker, name: 'Clicker' },
+    { path: '/modal', component: Modals, name: 'Modal' },
+    { path: '/todolist', component: ToDoList, name: 'To-Do List' },
+]
+
+  const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+let app = createApp(App);
+app.use(router);
+
+app.mount('#app');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+const glob = require('glob');
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +10,48 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.vue$/i,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.twig$/,
+        use: {
+          loader: 'twig-loader',
+          options: {
+              // See options section below
+          },
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    alias: {
+      vue:'vue/dist/vue.esm-bundler.js'
+    }
+  }
 };
